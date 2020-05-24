e16e1ea561f3c053b7ef95fe0aa4d64bc52c1eef
shnehna <mr_shenhan@outlook.com>
2020-05-24 22:52:18 +0800

    Initial commit

---
+++ src/main/java/com/shenhan/nicedays/NicedaysApplication.java
@@ -0,0 +1,13 @@
+package com.shenhan.nicedays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicedaysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NicedaysApplication.class, args);
+    }
+
+}
